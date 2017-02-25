eff6555111cde5bd767aa2411c2b2cbd00d2780f
serin9 <cheche_serin@naver.com>
2017-02-25 23:59:26 +0900

    [simple-tab] little by little

--- simple-tab/tab.js
+++ simple-tab/tab.js
@@ -1,9 +1,24 @@
 var locale = 'de-DE';
-var clock = {
+var content = {
 	msg: document.querySelector('.content .message').firstChild,
+  search: document.querySelector('.content .search').firstChild,
+  links: document.querySelector('.content .links').firstChild,
 };
+var footer = {
+  date: document.querySelector('.footer').firstChild
+}
 render();
 
 function render() {
-	clock.msg.data = "new tab";
+	content.msg.data = "new tab";
+
+  var now = new Date();
+  var seconds = now.getSeconds();
+  footer.date.data = now.toLocaleString(locale, {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  });
+
+  setTimeout(render, (60 - seconds) * 1000);
 }
