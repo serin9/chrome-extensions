2c6e638fc964ca4f34b85b055adff61a68302ade
serin9 <cheche_serin@naver.com>
2017-02-26 21:54:53 +0900

    [simple-tab] Add two search links

--- simple-tab/tab.js
+++ simple-tab/tab.js
@@ -1,9 +1,15 @@
 var locale = 'de-DE';
 var content = {
 	msg: document.querySelector('.content .message').firstChild,
-  search: document.querySelector('.content .search').firstChild,
+  //search: document.querySelector('.content .search').firstChild,
   links: document.querySelector('.content .links').firstChild,
 };
+var search_content = {
+  naver_input: document.getElementById("naver_search_input"),
+  naver_btn: document.getElementById("naver_search_btn"),
+  google_input: document.getElementById("google_search_input"),
+  google_btn: document.getElementById("google_search_btn")
+}
 var footer = {
   date: document.querySelector('.footer').firstChild
 }
@@ -21,4 +27,33 @@ function render() {
   });
 
   setTimeout(render, (60 - seconds) * 1000);
+
+  setEventListener();
+}
+
+function search(engine, query) {
+  if (engine == "naver") {
+    window.location.href = "https://search.naver.com/search.naver?where=nexearch%84&sm=top_hty&fbm=1&ie=utf8&query=" + encodeURIComponent(query);
+  } else if (engine == "google") {
+    window.location.href = "https://www.google.co.kr/?gfe_rd=cr&ei=gcmyWJzhAqz48AeInKroCg#q=" + encodeURIComponent(query);
+  }
+}
+
+function setEventListener() {
+  search_content.naver_input.addEventListener("keyup", function(event) {
+    if (event.which==13) {
+      search("naver", this.value)
+    }
+  });
+  search_content.naver_btn.addEventListener("click", function() {
+    search("naver", search_content.naver_input.value)
+  });
+  search_content.google_input.addEventListener("keyup", function() {
+    if (event.which==13) {
+      search("google", this.value)
+    }
+  });
+  search_content.google_btn.addEventListener("click", function() {
+    search("google", search_content.google_input.value)
+  });
 }
